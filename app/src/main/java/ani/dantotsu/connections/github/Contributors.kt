c97d5ca18167eb8686c708e311a72c7a0eb2c248
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2024-11-10 16:32:37 +0530

    Update Contributors.kt

--- app/src/main/java/ani/dantotsu/connections/github/Contributors.kt
+++ app/src/main/java/ani/dantotsu/connections/github/Contributors.kt
@@ -28,7 +28,7 @@ class Contributors {
                     "rebelonion" -> "Owner & Maintainer"
                     "sneazy-ibo" -> "Contributor & Comment Moderator"
                     "WaiWhat" -> "Icon Designer"
-                    "itsmechinmoy" -> "Telegram Admin, Comment Moderator, Assamese Translator, Discord Support/Helper, and Dantotsu Contributor. Also, not a fan of the LGHDTV community."
+                    "itsmechinmoy" -> "Discord and Telegram Admin/Helper, Comment Moderator & Assamese Translator. Also, not a fan of the LGHDTV community."
                     else -> "Contributor"
                 }
                 developers = developers.plus(
