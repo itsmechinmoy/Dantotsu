cde0f5f029b611efabb782f5cf2ff59cd511034d
aayush262 <aayushthakur262006@gmail.com>
2026-03-27 20:12:48 +0530

    migrate to Kotlin DSL and version catalogs (TOML)

---
+++ app/build.gradle.kts
@@ -0,0 +1,148 @@
+plugins {
+    alias(libs.plugins.android)
+    alias(libs.plugins.serialization)
+    alias(libs.plugins.ksp)
+
+    alias(libs.plugins.google)
+    alias(libs.plugins.crashlytics)
+}
+val gitCommitHash = providers.exec {
+    commandLine("git", "rev-parse", "--verify", "--short", "HEAD")
+}.standardOutput.asText.get().trim()
+
+android {
+    namespace = "ani.dantotsu"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "ani.dantotsu"
+        minSdk = 21
+        targetSdk = 36
+
+        versionName = "3.2.2"
+        versionCode = (versionName ?: "1.0.0").split(".")
+            //noinspection WrongGradleMethod
+            .map { it.toInt() * 100 }
+            .joinToString("")
+            .toInt()
+
+        signingConfig = signingConfigs.getByName("debug")
+    }
+
+    flavorDimensions += "store"
+
+    productFlavors {
+        create("fdroid") {
+            dimension = "store"
+            versionNameSuffix = "-fdroid"
+        }
+        create("google") {
+            dimension = "store"
+            isDefault = true
+        }
+    }
+
+    buildTypes {
+        create("alpha") {
+            applicationIdSuffix = ".beta1"
+            versionNameSuffix = "-alpha01-$gitCommitHash"
+            manifestPlaceholders["icon_placeholder"] = "@mipmap/ic_launcher_alpha"
+            manifestPlaceholders["icon_placeholder_round"] = "@mipmap/ic_launcher_alpha_round"
+            isDebuggable = true
+            isJniDebuggable = true
+            isMinifyEnabled = false
+            isShrinkResources = false
+            isDefault = true
+        }
+
+        getByName("debug") {
+            applicationIdSuffix = ".beta"
+            versionNameSuffix = "-beta01"
+            manifestPlaceholders["icon_placeholder"] = "@mipmap/ic_launcher_beta"
+            manifestPlaceholders["icon_placeholder_round"] = "@mipmap/ic_launcher_beta_round"
+            isDebuggable = false
+        }
+
+        getByName("release") {
+            manifestPlaceholders["icon_placeholder"] = "@mipmap/ic_launcher"
+            manifestPlaceholders["icon_placeholder_round"] = "@mipmap/ic_launcher_round"
+            isDebuggable = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-gson.pro",
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+    compilerOptions {
+        freeCompilerArgs.addAll(
+            "-Xcontext-receivers",
+            "-Xmulti-platform"
+        )
+    }
+}
+
+dependencies {
+
+    // Firebase
+    add("googleImplementation", platform(libs.firebase.bom))
+    add("googleImplementation", libs.bundles.firebase)
+
+    // AndroidX
+    implementation(libs.bundles.androidx)
+
+    // Kotlin
+    implementation(libs.kotlin.reflect)
+    implementation(libs.kotlin.stdlib)
+
+    // Core libs
+    implementation(libs.bundles.misc)
+
+    // Glide
+    implementation(libs.bundles.glide)
+    ksp(libs.glide.ksp)
+
+    implementation(libs.bundles.media3)
+    implementation(libs.bundles.subtitles)
+    implementation(libs.mediarouter)
+
+    // UI
+    implementation(libs.material)
+    implementation(files("libs/AnimatedBottomBar-7fcb9af.aar"))
+    implementation(libs.flexbox)
+    implementation(libs.kenburns)
+    implementation(libs.subsampling)
+    implementation(libs.gesture)
+    implementation(libs.ebook)
+    implementation(libs.dialogs)
+    implementation(libs.charts)
+
+    // Markwon
+    implementation(libs.bundles.markwon)
+
+    // Groupie
+    implementation(libs.bundles.groupie)
+
+    // Rx
+    implementation(libs.bundles.rx)
+
+    // OkHttp
+    implementation(libs.bundles.okhttp)
+
+    // Others
+    implementation(libs.okio)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,21 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
+
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
         maven("https://jitpack.io")
     }
 }
+
 rootProject.name = "Dantotsu"
 include(":app")
